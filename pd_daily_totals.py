61dd2f38664e462ae54971fe28c85d1c9a535036
Douglas Klos <dougklos@gmail.com>
2020-09-28 14:02:17 -0700

    Updated databae and functionality of pandas version

--- pd_daily_totals.py
+++ pd_daily_totals.py
@@ -14,6 +14,7 @@ S. Save database
 R. Sort database
 M. Add multiple daily entries
 T. Display daily sums
+DEL. Delete last row
 X. Exit
 >>> """
 
@@ -26,60 +27,76 @@ def get_input(_type, message, error):
             print(error)
 
 
-def get_date_time():
-    try:
-        _date = input("YYYY/MM/DD - ").replace(".", "/").replace(" ", "/").replace("-", "/")
-        _time = input("HH:MM - ").replace(".", ":").replace(" ", ":").replace("-", "/")
-
-        if _time == "":
-            return time.time()
-        if _date == "":
-            _date = datetime.date.today().strftime("%Y/%m/%d")
-
-        year, month, day = map(int, _date.split("/"))
-        hour, minute = map(int, _time.split(":"))
-        return datetime.datetime(year, month, day, hour, minute).timestamp()
-
-    except ValueError:
-        print("Bad date/time given")
-        return 0
-
-
 def add_multiple_entriesa():
     new_entries = []
-    _date = input("YYYY/MM/DD - ").replace(".", "/").replace(" ", "/").replace("-", "/")
-    if _date == "":
-        _date = datetime.date.today().strftime("%Y/%m/%d")
-    year, month, day = map(int, _date.split("/"))
+
+    year, month, day = get_valid_date()
+
     while True:
-        _time = input("HH:MM - ").replace(".", ":").replace(" ", ":").replace("-", "/")
-        if _time == "":
-            break
-        try:
-            hour, minute = map(int, _time.split(":"))
-        except ValueError:
-            hour = int(_time)
-            minute = 0
+        hour, minute = get_valid_time()
+
         value = get_input(float, "Enter dosage ", "Must be float value")
         epoch = datetime.datetime(year, month, day, hour, minute).timestamp()
-        new_entries.append({
-            "epoch": epoch,
-            "value": value,
-            "date_time": pd.to_datetime(epoch, unit="s", utc=True).tz_convert("US/Pacific"),
-        })
+        new_entries.append(
+            {
+                "epoch": epoch,
+                "value": value,
+                "date_time": pd.to_datetime(epoch, unit="s", utc=True).tz_convert("US/Pacific"),
+            }
+        )
+
+        _continue = input("Continue? [Y/n]: ")
+        if _continue.lower() not in ["", "y"]:
+            break
 
     return new_entries
 
 
+def get_valid_time():
+    while True:
+        _time = input("HH:MM - ").replace(".", ":").replace(" ", ":").replace("-", ":")
+        if _time == "":
+            _time = datetime.datetime.now().strftime("%H:%M")
+        try:
+            hour, minute = map(int, _time.split(":"))
+        except ValueError:
+            if _time[-1:] == ":":
+                _time = _time.strip(":")
+            hour = int(_time)
+            minute = 0
+        try:
+            datetime.time(int(hour), int(minute))
+            break
+        except ValueError:
+            print("Bad time")
+
+    return hour, minute
+
+
+def get_valid_date():
+    while True:
+        try:
+            _date = input("YYYY/MM/DD - ").replace(".", "/").replace(" ", "/").replace("-", "/")
+            if _date == "":
+                _date = datetime.date.today().strftime("%Y/%m/%d")
+            year, month, day = map(int, _date.split("/"))
+            datetime.datetime(int(year), int(month), int(day))
+            break
+        except ValueError:
+            print("Invalid date")
+
+    return year, month, day
+
+
 def pd_read_database(file_name):
     database = pd.read_csv(file_name)
-    database["date_time"] = pd.to_datetime(database.epoch, unit="s", utc=True).dt.tz_convert("US/Pacific")
+    database["date_time"] = pd.to_datetime(database.epoch, unit="s", utc=True).dt.tz_convert(
+        "US/Pacific"
+    )
     return database
 
 
 def pd_save_database(file_name, database):
-    # cols_to_keep = ["epoch", "value"]
-    # database[cols_to_keep].to_csv(file_name, index=False)
     database.to_csv(file_name, index=False)
 
 
@@ -104,15 +121,20 @@ def pd_display_database(database):
 
 
 def display_daily_totals(database):
-    daily_totals = database.set_index('date_time').groupby(pd.Grouper(freq='D')).sum().reset_index()
-    print(daily_totals[['date_time', 'value']])
+    daily_totals = database.set_index("date_time").groupby(pd.Grouper(freq="D")).sum().reset_index()
+    # print(daily_totals)
+    print(daily_totals[["date_time", "value"]])
+
+
+def delete_last_entry(database):
+    database.drop(database.tail(1).index,inplace=True) 
 
 
 def main():
     file_name = "database.csv"
     database = pd_read_database(file_name)
     pd.set_option("display.max_rows", None, "display.max_columns", None)
-    # pd.options.display.float_format = '{:.2f}'.format
+    pd.options.display.float_format = "{:.2f}".format
 
     while True:
         selection = input(menu())
@@ -121,10 +143,11 @@ def main():
             pd_display_database(database)
 
         elif selection.lower() == "a":
-            new_date_time = get_date_time()
-            if new_date_time != 0:
-                value = get_input(float, "Enter dosage ", "Must be float value")
-                database = add_entry(math.floor(new_date_time), str(value), database)
+            year, month, day = get_valid_date()
+            hour, minute = get_valid_time()
+            epoch = datetime.datetime(year, month, day, hour, minute).timestamp()
+            value = get_input(float, "Enter value ", "Must be float value")
+            database = add_entry(math.floor(epoch), str(value), database)
 
         elif selection.lower() == "s":
             pd_save_database(file_name, database)
@@ -132,14 +155,17 @@ def main():
         elif selection.lower() == "r":
             database = pd_sort_database(database)
 
-        elif selection.lower() == 'm':
+        elif selection.lower() == "m":
             database = database.append(add_multiple_entriesa(), ignore_index=True)
 
-        elif selection.lower() == 't':
+        elif selection.lower() == "t":
             display_daily_totals(database)
 
         elif selection.lower() == "x":
             return
+
+        elif selection.lower() == 'del':
+            delete_last_entry(database)
 
         else:
             pass
